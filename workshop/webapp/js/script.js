2a46daf190acade1e136d9b5f6d89614e00ec4ae
Isoye12 <0150371278i@gmail.com>
2024-08-26 13:43:45 +0900

    html

---
+++ workshop/webapp/js/script.js
@@ -0,0 +1,12 @@
+const click = () => {
+    window.open('../html/popup.html', '', 'width=560, height=600')
+    let num = document.getElementById()
+    switch (num) {
+        case '0':
+            
+    }
+}
+
+const close = () => {
+    window.close()
+}
