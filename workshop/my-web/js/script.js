de6ce3124d05ed30840058bdb3b2151642ee84a8
Isoye12 <0150371278i@gmail.com>
2024-08-26 15:14:23 +0900

    third

--- workshop/my-web/js/script.js
+++ workshop/my-web/js/script.js
@@ -1,10 +1,6 @@
 const click = () => {
     window.open('../html/popup.html', '', 'width=560, height=600')
     let num = document.getElementById()
-    switch (num) {
-        case '0':
-            
-    }
 }
 
 const close = () => {
